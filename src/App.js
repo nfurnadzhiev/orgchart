3666ffabd34987b07945f0d16e04312188546043
nikolay.furnadzhiev <nikolayvanev@gmail.com>
2018-11-06 14:44:57 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PostData from './PostData.js'
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <PostData />
+      </div>
+      )
+  }
+}
+
+export default App
